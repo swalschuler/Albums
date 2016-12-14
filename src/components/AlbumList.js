37f5c3919e309d1a02daa6d934b4dc2b3adc6f21
Schuyler Alschuler <Schuyler Alschuler>
2016-12-13 16:35:40 -0800

    Updated album list

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
 
+  state =  { albums: [] };
+
+
   componentWillMount() {
-    console.log("test");
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
   }
+
+  renderAlbums() {
+    return this.state.albums.map( album => 
+    <AlbumDetail key={album.title} album={album}/>
+    );
+  }
+
 // Debugger and Ctrl shift m to debug.
   render() {
     return (
       <View>
-        <Text>Album List.</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
